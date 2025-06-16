e14b0d5f82c51f7dba19746f3eab2ea4a0ca99fa
lucasoriental <lucasoriental16@gmail.com>
2025-06-16 14:10:37 +0100

    Add task routes and cookie-parser middleware to the application

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ import express from "express";
 import cors from "cors";
 
 import user from "./routes/user.routes.js";
-import "./config/env.config.js"
+import tasks from "./routes/tasks.routes.js";
+import "./config/env.config.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-const PORT = 3000 || process.env.PORT
+const PORT = 3000 || process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 app.use("/user", user);
+app.use("/tasks", tasks);
 
 app.listen(PORT, () => console.log("Server is running on PORT: ", PORT));
